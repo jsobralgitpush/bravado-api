55da601f68a1ddf14fa8d243f553072b25e043b3
jose mancebo <jose.sobral@poli.ufrj.br>
2022-05-17 19:55:42 -0300

    Add multiple allow to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -21,4 +21,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get]
   end
+
+  allow do
+    origins 'http://localhost:3001'
+
+    resource '*',
+      headers: :any,
+      methods: [:get]
+  end
 end
