43001d982b8578605b694b543137453da47e7c61
jose mancebo <jose.sobral@poli.ufrj.br>
2022-05-17 15:22:37 -0300

    Remove s from http on CORS origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://bravado-client.herokuapp.com'
+    origins 'http://bravado-client.herokuapp.com'
 
     resource '*',
       headers: :any,
